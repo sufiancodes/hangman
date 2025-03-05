34400ca58b541404063986d1cf46f4dd218ed062
Sufian <176370395+sufiancodes@users.noreply.github.com>
2025-03-05 16:57:29 +0500

    Add method to render hangman according to game
    the logic to render hangman based on player guesses along with gallows implement

--- lib/board.rb
+++ lib/board.rb
@@ -1,18 +1,49 @@
 class Board
-  attr_reader :secret_word
+  attr_reader :secret_word, :guesses
   def initialize
     @secret_word = ''
+    @guesses = 6
   end
   def load_random_secret_word
     file = File.open('words.txt')
     @secret_word = file.readlines[rand(9894)]
     file.close
   end
-  def render_views
-    dashes = secret_word.length - 1
-    dashes.times do
-      print "_ "
+  def render_hangman
+    if @guesses == 0
+      puts "``` |"
+    elsif @guesses == 1
+      puts " O  |"
+    elsif @guesses == 2
+      puts " O  |"
+      puts " |  |"
+    elsif @guesses == 3
+      puts " O  |"
+      puts "/|  |"
+    elsif @guesses == 4
+      puts " O  |"
+      puts "/|\\ |"
+    elsif @guesses == 5
+      puts " O  |"
+      puts "/|\\ |"
+      puts "/   |"
+    elsif @guesses == 6
+      puts " O  |"
+      puts "/|\\ |"
+      puts "/ \\ |"
     end
+  end
+  def render_gallows
+    puts "____"
+    puts " |  |"
+    render_hangman
+    3.times {puts "    |"}
+    puts "   / \\"
+  end
+  def render_views
+    render_gallows
+    dashes = secret_word.length - 1
+    dashes.times { print "_ "}
     puts 
   end
 end
