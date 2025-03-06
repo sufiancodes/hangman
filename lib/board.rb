faaf2c9f4bd68649c184cb11500814a982e531c2
Sufian <176370395+sufiancodes@users.noreply.github.com>
2025-03-06 22:07:49 +0500

    game file move to lib code improve
    game file is move to lib directory for convenience logic for putting out wrong word to display implement

--- lib/board.rb
+++ lib/board.rb
@@ -2,8 +2,10 @@ class Board
   attr_reader :secret_word, :guesses
   def initialize
     @secret_word = ''
-    @guesses = 6
-    @display_array = []
+    @total_guesses = 0
+    @display_dashes = []
+    @current_guess = ''
+    @incorrect_letters = []
   end
   def load_random_secret_word
     file = File.open('words.txt')
@@ -11,24 +13,24 @@ class Board
     file.close
   end
   def render_hangman
-    if @guesses == 0
+    if @total_guesses == 0
       puts "``` |"
-    elsif @guesses == 1
+    elsif @total_guesses == 1
       puts " O  |"
-    elsif @guesses == 2
+    elsif @total_guesses == 2
       puts " O  |"
       puts " |  |"
-    elsif @guesses == 3
+    elsif @total_guesses == 3
       puts " O  |"
       puts "/|  |"
-    elsif @guesses == 4
+    elsif @total_guesses == 4
       puts " O  |"
       puts "/|\\ |"
-    elsif @guesses == 5
+    elsif @total_guesses == 5
       puts " O  |"
       puts "/|\\ |"
       puts "/   |"
-    elsif @guesses == 6
+    elsif @total_guesses == 6
       puts " O  |"
       puts "/|\\ |"
       puts "/ \\ |"
@@ -36,9 +38,18 @@ class Board
   end
   def take_input
     puts "Enter the character"
-    guess = gets.chomp
-    @display_array = @secret_word.split('')
-    @display_array.pop
+    @guess = gets.chomp
+    if @secret_word.include? @guess
+      
+    else
+      @incorrect_letters.push(@guess)
+      @total_guesses += 1
+    end
+  end
+  def find_index
+    display_array = @secret_word.split('')
+    display_array.pop
+    index = display_array.find_index(@guess)
   end
   def render_gallows
     puts "____"
@@ -49,14 +60,20 @@ class Board
   end
   def render_views
     render_gallows
-    dashes = secret_word.length - 1
-    dashes.times { print "_ "}
-    puts 
+    puts "Here are incorrect letters"
+    p @incorrect_letters
+    @display_dashes = Array.new(@secret_word.length - 1,"_")
+    puts @display_dashes
   end
 end
 
 game = Board.new
 game.load_random_secret_word
 puts game.secret_word
+game.take_input
 game.render_views
-game.make_guess
+
+arr = ["a","b","c","d","e","f"]
+arr.each do |value|
+  print " #{value}"
+end
