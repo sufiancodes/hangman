4600dff2fe0a66551280fef44e5938374637117e
Sufian <176370395+sufiancodes@users.noreply.github.com>
2025-03-15 11:41:09 +0500

    method for saving game rename
    method to save game rename from serialize_object to save_serialize_object because thats what it do method to save serialized object is yet to implement

--- lib/player.rb
+++ lib/player.rb
@@ -3,7 +3,7 @@ class Player
   def initialize
     @name = ''
   end
-  def serialize_objects
+  def save_serialize_objects
     puts "whats the name of file"
     name = gets.chomp
     File.write("#{name}.json" , 'Hello, world')
@@ -11,7 +11,7 @@ class Player
   def save_the_game
     puts "Do you want to save the game"
     save_game = gets.chomp.downcase
-    serialize_objects if save_game == 'yes'
+    save_serialize_objects if save_game == 'yes'
   end
   def welcome
     puts "Welcome whats your name"
