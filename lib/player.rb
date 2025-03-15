ef17806b93d2fb8cf13842b42462deaa4ce5c53d
Sufian <176370395+sufiancodes@users.noreply.github.com>
2025-03-15 12:08:23 +0500

    Two method made private in player class
    method save serialized object and serialized object made private since they are only being use within

--- lib/player.rb
+++ lib/player.rb
@@ -3,11 +3,6 @@ class Player
   def initialize
     @name = ''
   end
-  def save_serialize_objects
-    puts "whats the name of file"
-    name = gets.chomp
-    File.write("#{name}.json" , 'Hello, world')
-  end
   def save_the_game
     puts "Do you want to save the game"
     save_game = gets.chomp.downcase
@@ -17,5 +12,14 @@ class Player
     puts "Welcome whats your name"
     @name = get.chomp
     puts "Let the game began #{@name}"
+  end
+  private
+  def serialized_objects
+    
+  end
+  def save_serialize_objects
+    puts "whats the name of file"
+    name = gets.chomp
+    File.write("#{name}.json" , 'Hello, world')
   end
 end
